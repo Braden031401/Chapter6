b7e567bfed34a93f36f26d2b03cd1e674b50a784
Braden031401 <rc179978@students.davenport.k12.ia.us>
2017-09-28 09:26:35 -0500

    FIRE!

---
+++ Chapter_6/src/gameZone/RandomGuessLoop.java
@@ -0,0 +1,47 @@
+package gameZone;
+
+import javax.swing.JOptionPane;
+
+public class RandomGuessLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+
+	
+		int guess;
+		int result;
+		String resultString;
+		String msg = null;
+		int low = 1;
+		int high = 100;
+		int score = 10;
+		result = low + (int)(Math.random() * high);
+		
+		do {
+			
+		resultString = JOptionPane.showInputDialog(null, "Try to guess my number between "+ low +" and "+ high);
+		guess = Integer.parseInt(resultString);
+		score = score -1;
+		if(guess==result)
+		{
+			msg="You Win!! ";
+		}
+			if (guess<result)
+		{
+			msg="Your guess was too low ";
+		
+		}
+			if (guess>result)
+				
+					msg="Your guess was too high ";
+		
+					JOptionPane.showMessageDialog(null, msg + "Your Score is "+ score);
+				
+	}while(guess != result && score !=0);
+	
+		
+			JOptionPane.showMessageDialog(null, "Game Over!");
+
+}
+}
