eb55db508b1662b3dccddd3b6d2493b180edfd85
Braden031401 <rc179978@students.davenport.k12.ia.us>
2017-10-02 09:56:19 -0500

    Wow, This debugging is hard.

---
+++ Chapter_6/src/exercises/TestScoresStatistics.java
@@ -0,0 +1,15 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class TestScoresStatistics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+int Score;
+int 
+		
+	}
+
+}
